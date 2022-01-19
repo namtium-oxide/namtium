5e5bdea4c318f5dcd2f15ed9e9d3685268428f75
Waritnan Sookbuntherng <lion328@hotmail.co.th>
2022-01-20 04:45:45 +0700

    Fix exceptions when GSON parse an empty URL

--- src/main/java/com/lion328/xenonlauncher/minecraft/launcher/json/data/DownloadInformation.java
+++ src/main/java/com/lion328/xenonlauncher/minecraft/launcher/json/data/DownloadInformation.java
@@ -24,13 +24,14 @@ package com.lion328.xenonlauncher.minecraft.launcher.json.data;
 
 import com.google.gson.annotations.SerializedName;
 
+import java.net.MalformedURLException;
 import java.net.URL;
 
 public class DownloadInformation
 {
 
     @SerializedName("url")
-    private URL url;
+    private String url;
     @SerializedName("sha1")
     private String sha1Hash;
     @SerializedName("size")
@@ -43,14 +44,16 @@ public class DownloadInformation
 
     public DownloadInformation(URL url, String sha1Hash, int sizeInBytes)
     {
-        this.url = url;
+        this.url = url.toString(); // TODO: restruct the code to store URL (or URI?)
         this.sha1Hash = sha1Hash;
         this.sizeInBytes = sizeInBytes;
     }
 
-    public URL getURL()
-    {
-        return url;
+    public URL getURL() throws MalformedURLException {
+        if (url == null || url.isEmpty()) {
+            return null;
+        }
+        return new URL(url);
     }
 
     public String getSHA1Hash()
